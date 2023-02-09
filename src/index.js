a70beb1bedd2f4dff1b8cfb65d719803e9372a0b
BaNgan <bangank36@gmail.com>
2023-02-09 22:08:48 +0700

    Update index.js
    Disregard chrome.runtime on index file

--- src/index.js
+++ src/index.js
@@ -7,8 +7,4 @@ import '@automattic/isolated-block-editor/build-browser/core.css';
 import '@automattic/isolated-block-editor/build-browser/isolated-block-editor.css';
 import './style.scss';
 
-chrome.runtime.onMessage.addListener( ( message ) => {
-	if ( message && message.type === 'TOGGLE_GUTENBERG' ) {
-		toggleAvailableEditors();
-	}
-} );
+toggleAvailableEditors();
